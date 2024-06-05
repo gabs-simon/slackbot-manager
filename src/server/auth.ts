7268bbface0fe252771b27c94c96883df5ae7a60
Gabs <6173452+gabs-simon@users.noreply.github.com>
2024-06-04 20:41:27 -0700

    user id is now number

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -26,7 +26,7 @@ import {
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
-      id: string;
+      id: number;
       // ...other properties
       // role: UserRole;
     } & DefaultSession["user"];
