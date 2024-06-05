9012da1c54036a6359ce8cdd5520f30628f3b5e9
Gabs Simonetti <gabs.simon@outlook.com>
2024-06-05 00:13:35 -0700

    Redirect to admin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
 import { redirect } from "next/navigation";
-import { getServerAuthSession } from "~/server/auth";
 
 export default async function Home() {
-  const session = await getServerAuthSession();
-  console.log({ session });
-
-  if (!session) {
-    redirect("/api/auth/signin");
-  }
   redirect("/admin/settings/general");
 }
